a83755b05c239cdd9194e9714b4a01d39debfb71
Jalaj Jain <jalajjain39@gmail.com>
2021-06-17 18:01:56 +0530

    add cocktail 1.0

---
+++ src/components/SearchForm.js
@@ -0,0 +1,40 @@
+import React,{useState} from 'react'
+import { useGlobalContext } from '../context'
+
+
+const SearchForm = () => {
+  const {setSearchTerm} = useGlobalContext();
+  const searchValue = React.useRef('');
+ 
+
+  React.useEffect(() => {
+    searchValue.current.focus()
+  },[]) 
+
+ const searchCocktail = () => {
+
+   setSearchTerm(searchValue.current.value)
+ }
+ const handleSubmit = (e) => {
+  e.preventDefault();
+
+ }
+ 
+  return (
+    <section className="section search">
+      <form onSubmit={handleSubmit} className="search-form">
+        <div className='form-control'>
+          <label htmlFor='name'>search cocktails</label>
+          <input 
+          type='text' 
+          id='name' 
+          ref={searchValue}
+          onChange={searchCocktail} /> 
+
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export default SearchForm
---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+      Jalaj this side , glad to see you here! Thanks for checking out my project :)
+         
+      </p>
+    </section>
+  )
+}
+
+export default About
